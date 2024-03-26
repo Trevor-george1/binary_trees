d0ceddc22e00e9e245498a77b328be2debedd412
Trevor <Trevormahinda@gmai.com>
2024-03-26 15:01:13 +0300

    create: sibling

---
+++ 17-binary_tree_sibling.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_sibling - finds sibling of a node
+ * @node: node
+ * Return: success
+ */
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+	if (node == NULL || node->parent == NULL)
+	{
+		return (NULL);
+	}
+	if (node->parent->left == node)
+		return (node->parent->right);
+	return (node->parent->left);
+}
