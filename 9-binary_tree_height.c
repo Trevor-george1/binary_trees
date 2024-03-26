6baeba66c69f003c265ffa23f6690884551a6849
Trevor <Trevormahinda@gmai.com>
2024-03-26 12:04:23 +0300

    height of tree

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -6,17 +6,20 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t left_depth;
-	size_t right_depth;
+	size_t l = 0;
+	size_t r = 0;
 
 	if (tree == NULL)
-		return (0);
-	left_depth = binary_tree_height(tree->left);
-	right_depth = binary_tree_height(tree->right);
-	if (left_depth > right_depth)
 	{
-		return (left_depth + 1);
+		return (0);
 	}
 	else
-		return (right_depth + 1);
+	{
+		if (tree)
+		{
+			l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+			r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+		}
+		return ((l > r) ? l : r);
+	}
 }
